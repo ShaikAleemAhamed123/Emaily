b2a69ae89c1f2ca11e57647da3148451eca28d2d
Aleem Ahamed <shaikaleemahamed786@gmail.com>
2022-06-27 19:09:29 +0530

    .

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,10 +15,7 @@ app.get('/',(req,res)=>{
     res.send(req.user);
   })
 
-  app.get('/auth/google/callback',()=>{
-    passport.authenticate('google');
-    res.send("Ok Done !")
-  });
+  app.get('/auth/google/callback', passport.authenticate('google'));
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
